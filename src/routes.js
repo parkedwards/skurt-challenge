f604d74dc610a3d8072e9c8b452023c39667f182
Edward Park <ed.sh.park@gmail.com>
2017-03-20 17:03:03 -0700

    refactored to modularize routing file; fixed entry point

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+// import components here
+import Dashboard from './views/Dashboard';
+
+export default (
+  <Route path='/' component={Dashboard}>  
+  </Route>
+);

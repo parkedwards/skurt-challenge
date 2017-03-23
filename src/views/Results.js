8d86aaf5548324a133c58ea5d001e1f0ef0d0a3d
Edward Park <ed.sh.park@gmail.com>
2017-03-23 00:23:53 -0700

    created conditional renders for results page, depending on if there's a delay

--- src/views/Results.js
+++ src/views/Results.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { DELAY_THRESHOLD } from '../utils/utilFunctions';
 
-const Results = () => {
+const Results = ({ isDelayed, timeDelayed }) => {
+  if (!isDelayed) {
+    return (
+      <div>
+        <h3>You're chillin and on time!</h3>
+        <p>Your flight will land within {DELAY_THRESHOLD} minutes of schedule</p>
+        <Link to='/' className='btn btn-success'>
+          {'< '}Check Another Flight
+      </Link>
+      </div>
+    );
+  }
+
   return (
     <div>
-      This is the results view!
+      <h2>Flight Delayed by <span style={lateStyle}>{timeDelayed} minutes!</span></h2>
+      <Link to='/' className='btn btn-success'>
+        {'< '}Check Another Flight
+      </Link>
     </div>
   );
+};
+
+const lateStyle = {
+  color: 'red',
+  fontWeight: 'bold'
 };
 
 export default Results;

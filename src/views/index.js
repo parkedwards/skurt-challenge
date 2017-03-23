9b721ca63cbf07eaca599cb507c9a07d9706351b
Edward Park <ed.sh.park@gmail.com>
2017-03-23 00:23:25 -0700

    created component index to reference

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+// Index for view components folder
+
+export { default as Dashboard } from './Dashboard';
+export { default as EntryForm } from './EntryForm';
+export { default as Error } from './Error';
+export { default as Lookup } from './Lookup';
+export { default as Results } from './Results';
